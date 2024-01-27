94af28b0bbb549dc29c6b0f9e2cd1e0de41ffa37
francisrjs <franrojas331@gmail.com>
2024-01-27 13:10:01 -0300

    Pizarra se borra segun el rango y cambios de css

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ let gridSize = 100;
 
 // creo los cuadrados
 function createGrid() {
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        var gridSquare = document.createElement("div");
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    var gridSquare = document.createElement("div");
 
-        // agrego los atributos y clases
-        gridSquare.className = "grid_dynamic";
-        gridSquare.innerHTML = '-';
+    // agrego los atributos y clases
+    gridSquare.className = "grid_dynamic";
+    gridSquare.innerHTML = "-";
 
-        // Le agrego un evento
-        gridSquare.addEventListener("mouseenter", function () {
-            this.style.backgroundColor = "white";
-        });
+    // Le agrego un evento
+    gridSquare.addEventListener("mouseenter", function () {
+      this.style.backgroundColor = "white";
+      this.style.color = "white";
+    });
 
-        // Ajusto el estilo con la corrección en las comillas
-        gridSquare.style.width = `calc(100% / ${gridSize})`;
-        gridSquare.style.height = `calc(100% / ${gridSize})`;
+    // Ajusto el estilo con la corrección en las comillas
+    gridSquare.style.width = `calc(100% / ${gridSize})`;
+    gridSquare.style.height = `calc(100% / ${gridSize})`;
 
-        // Adjunto el cuadrado al contenedor correcto
-        grid.appendChild(gridSquare);
-    }
+    // Adjunto el cuadrado al contenedor correcto
+    grid.appendChild(gridSquare);
+  }
 }
-
+function removeGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+}
 createGrid();
 
 function rangeSlide(value) {
-    document.getElementById('rangeValue').innerHTML = value;
-    gridSize=value;
-    createGrid();
+  document.getElementById("rangeValue").innerHTML = value;
+  gridSize = value;
+  removeGrid();
+  createGrid();
 }
